d92025532050423e2ed92c3233da535df4ff085e
hgrano <huw.grano@gmail.com>
2023-11-26 10:46:41 +1100

    add additional functions

--- uniswap-clone/src/Pair.sol
+++ uniswap-clone/src/Pair.sol
@@ -1,12 +1,31 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.21;
 
-import { ERC20 } from "solady/tokens/ERC20.sol";
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ERC20} from "solady/tokens/ERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-contract Pair is ERC20 {
+contract Pair is ERC20, ReentrancyGuard {
     using SafeERC20 for IERC20;
+
+    error SwapDoesNotMeetMinimumOut();
+    error InsufficientBalanceToRemoveLiquidity();
+    error RemoveLiquidityDoesNotMeetMinimum0Out();
+    error RemoveLiquidityDoesNotMeetMinimum1Out();
+    error AddLiquidityDoesNotMeetMinimumAmount0();
+    error AddLiquidityDoesNotMeetMinimumAmount1();
+
+    event Mint(address indexed sender, uint256 amount0, uint256 amount1);
+    event Burn(address indexed sender, uint256 amount0, uint256 amount1, address indexed to);
+    event Swap(
+        address indexed sender,
+        uint256 amount0In,
+        uint256 amount1In,
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address indexed to
+    );
 
     string private _name;
     string private _symbol;
@@ -35,40 +54,133 @@ contract Pair is ERC20 {
         _token1 = token1_;
     }
 
-    function _swapExactTokenForToken(bool side, uint amountIn, uint amoutOutMin) private {
-        address from;
+    function addLiquidity(
+        uint256 amount0Approved,
+        uint256 amount1Approved,
+        uint256 amount0Min,
+        uint256 amount1Min,
+        address to
+    ) external nonReentrant {
+        uint256 reserve0_ = _reserve0;
+        uint256 reserve1_ = _reserve1;
+        uint256 amount1ImpliedByApproval = (reserve1_ * amount0Approved) / reserve0_;
+        uint256 amount0ToUse;
+        uint256 amount1ToUse;
+        if (amount1ImpliedByApproval > amount1Approved) {
+            amount0ToUse = (reserve0_ * amount1Approved) / reserve1_;
+            if (amount0ToUse < amount0Min) {
+                revert AddLiquidityDoesNotMeetMinimumAmount0();
+            }
+            amount1ToUse = amount1Approved;
+        } else {
+            amount1ToUse = amount1ImpliedByApproval;
+            if (amount1ToUse < amount1Min) {
+                revert AddLiquidityDoesNotMeetMinimumAmount1();
+            }
+            amount0ToUse = amount0Approved;
+        }
+    
+        address token0_ = _token0;
+        address token1_ = _token1;
+
+        uint256 initialBalance0 = IERC20(token0_).balanceOf(address(this));
+        IERC20(token0_).safeTransferFrom(msg.sender, address(this), amount0ToUse);
+        uint256 actualAmount0 = IERC20(token0_).balanceOf(address(this)) - initialBalance0;
+
+        uint256 initialBalance1 = IERC20(token1_).balanceOf(address(this));
+        IERC20(token1_).safeTransferFrom(msg.sender, address(this), amount1ToUse);
+        uint256 actualAmount1 = IERC20(token1_).balanceOf(address(this)) - initialBalance1;
+
+        _reserve0 += uint112(actualAmount0);
+        _reserve1 += uint112(actualAmount1);
+        uint256 totalSupply_ = totalSupply();
+        uint256 liquidity0 = (actualAmount0 * totalSupply_) / reserve0_;
+        uint256 liquidity1 = (actualAmount1 * totalSupply_) / reserve1_;
+        if (liquidity0 < liquidity1) {
+            _mint(to, liquidity0);
+        } else {
+            _mint(to, liquidity1);
+        }
+    }
+
+    function removeLiquidity(uint256 liquidity, uint256 amount0Min, uint256 amount1Min, address to)
+        external
+        nonReentrant
+    {
+        uint256 balance = balanceOf(msg.sender);
+        if (balance < liquidity) {
+            revert InsufficientBalanceToRemoveLiquidity();
+        }
+        uint256 totalSupply_ = totalSupply();
+        address token0_ = _token0;
+        uint256 amount0 = (liquidity * IERC20(token0_).balanceOf(address(this))) / totalSupply_;
+        if (amount0 < amount0Min) {
+            revert RemoveLiquidityDoesNotMeetMinimum0Out();
+        }
+        address token1_ = _token1;
+        uint256 amount1 = (liquidity * IERC20(token1_).balanceOf(address(this))) / totalSupply_;
+        if (amount1 < amount1Min) {
+            revert RemoveLiquidityDoesNotMeetMinimum1Out();
+        }
+        _burn(msg.sender, liquidity);
+        IERC20(token0_).transfer(to, amount0);
+        IERC20(token1_).transfer(to, amount1);
+
+        emit Burn(msg.sender, amount0, amount1, to);
+    }
+
+    function swapExactToken0ForToken1(uint256 amountIn, uint256 amoutOutMin, address to) external nonReentrant {
+        _swapExactTokenForToken(false, amountIn, amoutOutMin, to);
+    }
+
+    function swapExactToken1ForToken0(uint256 amountIn, uint256 amoutOutMin, address to) external nonReentrant {
+        _swapExactTokenForToken(true, amountIn, amoutOutMin, to);
+    }
+
+    function _swapExactTokenForToken(bool side, uint256 amountIn, uint256 amoutOutMin, address to) private {
+        address fromToken;
         uint112 fromReserve;
-        address to;
+        address toToken;
         uint112 toReserve;
 
         if (side) {
-            from = _token1;
+            fromToken = _token1;
             fromReserve = _reserve1;
-            to = _token0;
+            toToken = _token0;
             toReserve = _reserve0;
         } else {
-            from = _token0;
+            fromToken = _token0;
             fromReserve = _reserve0;
-            to = _token1;
+            toToken = _token1;
             toReserve = _reserve1;
         }
 
-        uint256 initialBalanceFrom = IERC20(from).balanceOf(address(this));
-        uint256 initialBalanceTo = IERC20(to).balanceOf(address(this));
-        IERC20(from).safeTransferFrom(msg.sender, address(this), amountIn);
-        uint256 finalBalanceFrom = IERC20(from).balanceOf(address(this));
+        uint256 initialBalanceFrom = IERC20(fromToken).balanceOf(address(this));
+        uint256 initialBalanceTo = IERC20(toToken).balanceOf(address(this));
+        IERC20(fromToken).safeTransferFrom(msg.sender, address(this), amountIn);
+        uint256 finalBalanceFrom = IERC20(fromToken).balanceOf(address(this));
         uint256 actualAmountIn = finalBalanceFrom - initialBalanceFrom;
         uint256 actualAmountInWithFee = actualAmountIn * multiplerWithFee;
 
-        uint256 amountOut = actualAmountInWithFee * toReserve / (fromReserve * decimalMultiplier + actualAmountInWithFee);
+        uint256 amountOut =
+            actualAmountInWithFee * toReserve / (fromReserve * decimalMultiplier + actualAmountInWithFee);
         require(amountOut >= amoutOutMin, "Amount out must meet the slippage threshold");
-        IERC20(to).safeTransfer(msg.sender, amountOut);
-        uint256 finalBalanceTo = IERC20(to).balanceOf(address(this));
+        IERC20(toToken).safeTransfer(to, amountOut);
+        uint256 finalBalanceTo = IERC20(toToken).balanceOf(address(this));
         uint256 actualAmountOut = finalBalanceTo - initialBalanceTo;
 
         if (side) {
             _reserve1 += uint112(actualAmountIn);
             _reserve0 -= uint112(actualAmountOut);
+
+            // emit Swap(
+            //     msg.sender,
+            //     uint amount0In,
+            //     uint amount1In,
+            //     uint amount0Out,
+            //     uint amount1Out,
+            //     address indexed to
+            // );
         } else {
             _reserve0 += uint112(actualAmountIn);
             _reserve1 -= uint112(actualAmountOut);
